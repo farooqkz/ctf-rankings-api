4de679145d477bdc86217a5a190cc1e4114f6a92
Farooq Karimi Zadeh <fkz@riseup.net>
2023-04-13 19:28:41 +0330

    almost there

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::env::{args, current_dir};
-use anyhow::Result;
 use std::fs::read;
 use std::path::PathBuf;
 use std::str::from_utf8;
-use tide::Request;
-use tide::prelude::*;
+use tide::{Request, Response};
+use redis::{Connection, Commands};
+use serde::Deserialize;
 
+#[derive(Deserialize)]
 struct Config {
     redis_addr: String,
     listen_addr: String,
@@ -29,13 +30,22 @@ fn main() -> anyhow::Result<()> {
     let client = redis::Client::open(config.redis_addr.as_str())?;
     app.at("/:mode/:username").get(|req| { get_ranking_for_mode(req, client.get_connection()?) });
     app.at("/:username").get(|req| { get_ranking_all_modes(req, client.get_connection()?) });
-    app.listen(config.listen_addr)?;
+    app.listen(config.listen_addr);
+    Ok(())
 }
 
-async fn get_ranking_for_mode(req: Request<()>, connection: _) -> tide::Result<String> {
+fn get_ranking_for_mode(req: Request<()>, connection: Connection) -> tide::Result<String> {
+    let mode = req.param("mode")?;
+    let username = req.param("username")?;
 
+    let result = connection.get(format!("{mode}|{username}"))?;
+    if result == "" {
+        Ok(Response::builder(404).build())
+    } else {
+        Ok(Response::builder(200).body(result).content_type("application/json").build())
+    }
 }
 
-async fn get_ranking_all_modes(req: Request<()>, connection: _) -> tide::Result<String> {
-
+fn get_ranking_all_modes(req: Request<()>, connection: Connection) -> tide::Result<String> {
+    Ok("".to_string())
 }
