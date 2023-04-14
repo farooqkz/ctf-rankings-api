d79740ac7177a04fd87ba4a51b7e08c7f614b8e8
Farooq Karimi Zadeh <fkz@riseup.net>
2023-04-14 14:13:02 +0330

    just one more step

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
+use redis::{Commands, Connection};
+use serde::Deserialize;
 use std::env::{args, current_dir};
 use std::fs::read;
 use std::path::PathBuf;
 use std::str::from_utf8;
+use tide::prelude::*;
 use tide::{Request, Response};
-use redis::{Connection, Commands};
-use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Config {
     redis_addr: String,
     listen_addr: String,
 }
 
-fn main() -> anyhow::Result<()> {
+#[async_std::main]
+async fn main() -> tide::Result<()> {
     let config: Config;
     {
         let mut config_file_path = current_dir()?.join("config.toml");
         if let Some(arg) = args().nth(1) {
             if arg == "-h" || arg == "--help" {
-                println!("Usage: {} [/path/to/config/file.toml]", args().nth(0).unwrap());
+                println!(
+                    "Usage: {} [/path/to/config/file.toml]",
+                    args().nth(0).unwrap()
+                );
                 return Ok(());
             }
             config_file_path = PathBuf::from(arg);
@@ -27,25 +32,28 @@ fn main() -> anyhow::Result<()> {
     }
 
     let mut app = tide::new();
-    let client = redis::Client::open(config.redis_addr.as_str())?;
-    app.at("/:mode/:username").get(|req| { get_ranking_for_mode(req, client.get_connection()?) });
-    app.at("/:username").get(|req| { get_ranking_all_modes(req, client.get_connection()?) });
+    let client = redis::Client::open(config.redis_addr.clone().as_str())?;
+    app.at("/:mode/:username")
+        .get(|req| async { 
+            let mut con = client.get_connection()?;
+            get_ranking_for_mode(req, con) 
+        });
     app.listen(config.listen_addr);
     Ok(())
 }
 
-fn get_ranking_for_mode(req: Request<()>, connection: Connection) -> tide::Result<String> {
+fn get_ranking_for_mode(req: Request<()>, mut connection: Connection) -> tide::Result {
     let mode = req.param("mode")?;
     let username = req.param("username")?;
-
-    let result = connection.get(format!("{mode}|{username}"))?;
+    let result = connection
+        .get(format!("{mode}|{username}"))
+        .unwrap_or("".to_string());
     if result == "" {
         Ok(Response::builder(404).build())
     } else {
-        Ok(Response::builder(200).body(result).content_type("application/json").build())
+        Ok(Response::builder(200)
+            .body(result)
+            .content_type("application/json")
+            .build())
     }
-}
-
-fn get_ranking_all_modes(req: Request<()>, connection: Connection) -> tide::Result<String> {
-    Ok("".to_string())
 }
